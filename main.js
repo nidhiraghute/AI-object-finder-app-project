ebcab93029c6776b51212f24e149d9f995c73127
nidhiraghute <85737394+nidhiraghute@users.noreply.github.com>
2022-03-15 15:57:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+status = "";
+
 function start(){
+    objectDetector = ml5.objectDetector('cocossd', modalLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+   }
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
     
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+    
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status = true;
 }
